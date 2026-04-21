ef8bdf2168c292619e07a9b16586249c867da976
Karan Sanghvi <karansanghvi1303@gmail.com>
2026-04-21 10:58:55 -0700

    changed the app settings from create-react-app to vite, and removed the chatbot which was created previously

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import Layout from "./pages/Layout";
 import AboutUs from "./pages/AboutUs";
 import Services from "./pages/Services";
 import Programs from "./pages/Programs";
 import SocialWork from "./pages/SocialWork";
 import ContactUs from "./pages/ContactUs";
-import Chatbot from "./components/ChatBot";
 
 function App() {
   return (
@@ -20,7 +18,6 @@ function App() {
             <Route path="programs" element={<Programs/>}/>
             <Route path="socialwork" element={<SocialWork/>}/>
             <Route path="contactus" element={<ContactUs/>}/>
-            <Route path="chatbot" element={<Chatbot/>}/>
         </Routes>
       </BrowserRouter>
     </>
